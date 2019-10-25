80209dd20cefff8da6b463c81e98114f0a812747
Wouter Vermeij <wouter.vermeij@elements.nl>
2019-10-25 14:42:50 +0200

    formatting code

--- bootSimulator.plugin.zsh
+++ bootSimulator.plugin.zsh
@@ -13,25 +13,25 @@ bootSimulator() {
     devicesForVersion=$(mktemp)
     trap "{ rm -f $devicesForVersion; }" EXIT
 
-    xcrun simctl list > $allDevices
+    xcrun simctl list >$allDevices
 
-    grep -E '\-\- iOS .... \-\-' $allDevices | tr -d \- > $iosVersions
+    grep -E '\-\- iOS .... \-\-' $allDevices | tr -d \- >$iosVersions
 
-    numberOfVersions=$(wc -l < $iosVersions)
+    numberOfVersions=$(wc -l <$iosVersions)
 
-    if [ $numberOfVersions -eq 1 ] ; then
+    if [ $numberOfVersions -eq 1 ]; then
         iosVersion=$(head -$choice $iosVersions | tail -1)
-    else 
+    else
         echo "What iOS version do you want to run?"
 
         n=1
-        while read line; do 
+        while read line; do
             echo $n. $line
-            n=$((n+1))
-        done < $iosVersions
-        
+            n=$((n + 1))
+        done <$iosVersions
+
         printf 'iOS version: '
-        read tmp 
+        read tmp
         iosVersion=$(head -$tmp $iosVersions | tail -1)
     fi
     iosVersion="--$iosVersion--"
@@ -39,27 +39,27 @@ bootSimulator() {
     isChosenVersion=0
     while read line; do
         # If the next line contains '--' a different apple OS list started, we quit out
-        if [[ $line == *--* ]] && [ $line != $iosVersion ] ; then 
+        if [[ $line == *--* ]] && [ $line != $iosVersion ]; then
             isChosenVersion=0
         fi
 
         # If we reached the iOS version selected, add the line to the file
-        if [ $line = $iosVersion ] || [ $isChosenVersion -eq 1 ] ; then
+        if [ $line = $iosVersion ] || [ $isChosenVersion -eq 1 ]; then
             isChosenVersion=1
             # Append every device of the chosen OS in a file, except if it already booted
-            if [ $line != $iosVersion ] && [[ $line != *"Booted"* ]] ; then
-                echo $line >> $devicesForVersion
+            if [ $line != $iosVersion ] && [[ $line != *"Booted"* ]]; then
+                echo $line >>$devicesForVersion
             fi
         fi
-    done < $allDevices
+    done <$allDevices
 
     echo $iosVersion
 
     n=1
-    while read line; do 
+    while read line; do
         echo $n. $line | gsed -E "s/\([A-Z0-9]{8}(-[A-Z0-9]{4}){3}-[A-Z0-9]{12}\).*//g"
-        n=$((n+1))
-    done < $devicesForVersion
+        n=$((n + 1))
+    done <$devicesForVersion
 
     # Get user input
     printf 'Device number: '
